b35416b264218485eae9f394aed1117aec2e3bab
TJ VanToll <tj.vantoll@gmail.com>
2017-06-20 12:24:05 -0700

    Service updates

--- app/pokemon/pokemon.service.ts
+++ app/pokemon/pokemon.service.ts
@@ -19,4 +19,15 @@ export class PokemonService {
           });
     });
   }
+
+  types() {
+    return [
+      { type:"Poison", count: 59 }, { type:"Grass", count: 84 }, { type: "Fire",count: 56 },
+      { type: "Flying", count: 90 }, { type:"Water", count: 118 },{ type: "Bug", count: 66 },
+      { type: "Normal", count: 97 }, { type: "Electric", count: 42 }, { type: "Ground", count: 60 },
+      { type: "Fairy", count: 35 }, { type:"Fighting", count: 44 }, { type: "Psychic", count: 74 },
+      { type: "Rock", count: 55 }, { type:"Steel", count: 41 }, { type: "Ice", count: 33 },
+      { type: "Ghost", count: 35 }, { type: "Dragon", count: 38}, { type:"Dark", count: 44 }
+    ];
+  }
 }
