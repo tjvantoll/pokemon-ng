e7e418d646c265e83864d8167e8ce50c23bc7d2a
TJ VanToll <tj.vantoll@gmail.com>
2017-08-21 15:46:39 -0400

    Saving off the starting point...hopefully

--- app/detail.component.ts
+++ app/detail.component.ts
@@ -1,18 +1,11 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from '@angular/router';
-import * as SocialShare from "nativescript-social-share";
 
 @Component({
   selector: "ns-app",
   template: `
 
-<ActionBar class="action-bar" [title]="pokemon.name">
-  <ActionItem text="Share" (tap)="share(event, pokemon)" android.systemIcon="ic_menu_share_holo_dark" ios.systemIcon="9" ios.position="right"></ActionItem>
-</ActionBar>
-
-<FlexboxLayout justifyContent="center">
-  <Image [src]="pokemon.sprite"></Image>
-</FlexboxLayout>
+<ActionBar class="action-bar" [title]="pokemon.name"></ActionBar>
 `
 })
 
@@ -26,9 +19,5 @@ export class DetailComponent implements OnInit {
     this.route.params.forEach(params => {
       this.pokemon = params;
     })
-  }
-
-  share(event, pokemon) {
-    SocialShare.shareImage(pokemon.sprite);
   }
 }
--- app/list.component.ts
+++ app/list.component.ts
@@ -1,7 +1,4 @@
-import { Component, OnInit } from "@angular/core";
-import { RouterExtensions } from "nativescript-angular/router";
-
-import { PokemonService } from "./pokemon/pokemon.service";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "ns-app",
@@ -9,35 +6,9 @@ import { PokemonService } from "./pokemon/pokemon.service";
   template: `
 
 <ActionBar class="action-bar" title="Pokémon!"></ActionBar>
-
-<ScrollView>
-  <FlexboxLayout class="container">
-    <Image *ngFor="let pokemon of pokemonList"
-      [src]="pokemon.sprite"
-      (tap)="onTap($event, pokemon)"
-      ></Image>
-  </FlexboxLayout>
-</ScrollView>
 `
 })
 
-export class ListComponent implements OnInit {
-  pokemonList;
+export class ListComponent {
 
-  constructor(private router: RouterExtensions, private pokemonService: PokemonService) {}
-
-  ngOnInit() {
-    this.pokemonService.list().then((data) => {
-      this.pokemonList = data;
-    });
-  }
-
-  onTap(event, pokemon) {
-    this.router.navigate(["/detail", pokemon], {
-      transition: {
-        name: "curlUp",
-        duration: 500
-      }
-    });
-  }
 }
